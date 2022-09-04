4d1ab8cb9634b7847b4479a4867a054442b5e71b
Polobear9 <lieams1996@gmail.com>
2022-09-05 00:30:21 +0900

    Change the code be simple and more safely.

--- src/GameTools/User.java
+++ src/GameTools/User.java
@@ -3,24 +3,35 @@ package GameTools;
 import java.util.Scanner;
 
 public class User {
-	Scanner sc = new Scanner(System.in);
-	private int userPoint;
-	private String userName;
-	
-	//if user, use a this class. user must set a Name userself.
-	User(){
-		System.out.println("Place Set the User Name : ");
-		String userName = sc.nextLine();
-		this.userName = userName;
-	}
-	
-	public int getUserPoint() {
-		return userPoint;
-	}
-	public void addTheUserPoint(int userPoint) {
-		this.userPoint += userPoint;
-	}
-	public String getUserName() {
-		return userName;
-	}
+    Scanner sc = new Scanner(System.in);
+    private int userPoint;
+    private String userName;
+
+    //if user, use a this class. user must set a Name userself.
+    User() {
+        System.out.println("Place Set the User Name : ");
+        String userName = sc.nextLine();
+        this.setUserName(userName);
+    }
+
+    public int getUserPoint() {
+        return userPoint;
+    }
+
+    public void setUserPoint(int userPoint) {
+        this.userPoint = userPoint;
+    }
+
+    public void addTheUserPoint(int userPoint) {
+        this.setUserPoint(this.getUserPoint() + userPoint);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
+
